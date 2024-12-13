e6ed0efaeadb3e78bb283cc3123c7b96e984f40e
Amirreza Nazemi <amirrnazemi@lines.so>
2024-12-13 09:34:28 +0330

    edit keyboards

--- handlers/auth.py
+++ handlers/auth.py
@@ -8,6 +8,7 @@ from services.trello_service import TrelloClient
 from models.map_list import Map_List
 from models.map_card import Map_Card
 from config import TRELLO_API_KEY, TRELLO_TOKEN
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 auth_router = Router()
 
@@ -20,15 +21,14 @@ async def show_boards(message: types.Message, user: User):
     
     trello_client = TrelloClient(api_key=TRELLO_API_KEY, token=TRELLO_TOKEN)
     boards = await trello_client.get_boards(user_id=trello_user_id)
-    
     if not boards:
         await message.answer("بردی یافت نشد.")
         return
     
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = []
     for board in boards:
-        keyboard.add(types.InlineKeyboardButton(text=board['name'], callback_data=f"show_lists:{board['id']}"))
-    
+        keyboard.append([types.InlineKeyboardButton(text=board['name'], callback_data=f"show_lists:{board['id']}")])
+    keyboard = InlineKeyboardBuilder(keyboard).as_markup()
     await message.answer("لیست بردهای شما:", reply_markup=keyboard)
 
 # مثال: ایجاد یک نقشه لیست
@@ -43,10 +43,10 @@ async def show_lists(callback_query: types.CallbackQuery, user: User):
         await callback_query.message.answer("لیستی یافت نشد.")
         return
     
-    keyboard = types.InlineKeyboardMarkup()
+    keyboard = []
     for lst in lists:
-        keyboard.add(types.InlineKeyboardButton(text=lst['name'], callback_data=f"map_list:{lst['id']}"))
-    
+        keyboard.append([types.InlineKeyboardButton(text=lst['name'], callback_data=f"map_list:{lst['id']}")])
+    keyboard = InlineKeyboardBuilder(keyboard).as_markup()
     await callback_query.message.answer("لیست‌های این برد:", reply_markup=keyboard)
 
 @auth_router.callback_query(F.data.startswith("map_list:"))
@@ -57,9 +57,9 @@ async def map_list(callback_query: types.CallbackQuery, user: User):
     mapped_category = "Default Category"
     
     # ذخیره نقشه لیست
-    map_list = await Map_List.find_one(Map_List.telegram_id == user.telegram_id, Map_List.trello_list_id == list_id)
+    map_list = await Map_List.find_one(Map_List.trello_list_id == list_id)
     if not map_list:
-        map_list = Map_List(telegram_id=user.telegram_id, trello_list_id=list_id, mapped_category=mapped_category)
+        map_list = Map_List(name='test', trello_list_id=list_id, mapped_category=mapped_category)
     else:
         map_list.mapped_category = mapped_category
     await map_list.save()
